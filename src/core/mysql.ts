90e281d239551c3d398a3e5f443ac90203d710f8
bradleykenny <bkenny283@gmail.com>
2020-03-29 15:18:16 +1100

    Set up function for mySQL but not sure if it works yet.

--- src/core/mysql.ts
+++ src/core/mysql.ts
@@ -1,10 +1,13 @@
 import { createConnection } from 'mysql';
 
-var connection = createConnection({
-	host: 'localhost',
-	user: 'me',
-	password: 'my-secret-pw',
-	database: 'communicate'
-});
-
-connection.connect();
+export function sqlSetup(): object {
+	var connection = createConnection({
+		host: 'localhost',
+		user: 'me',
+		password: 'my-secret-pw',
+		database: 'communicate'
+	});
+	
+	connection.connect();
+	return connection;
+};
