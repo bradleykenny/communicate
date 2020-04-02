5019355e925f0b014ee288439d3d9bc84f1329be
bradleykenny <bkenny283@gmail.com>
2020-04-03 10:44:36 +1100

    Some more state changing.

--- client/src/Messages.tsx
+++ client/src/Messages.tsx
@@ -39,6 +39,9 @@ class Messages extends Component {
 
 class Message extends Component {
 	state = {
+		sender: 'Bradley Kenny',
+		time: '8:08pm',
+		title: 'Information on AirPods Pro',
 		text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque pretium ex quam, vitae aliquet erat lobortis lobortis. Aliquam lacus ante, ultrices quis tincidunt id, lacinia at diam. Vestibulum id pulvinar massa. Cras eu nulla quis quam scelerisque vulputate vitae ut arcu. Suspendisse tristique non massa et pretium. Maecenas nisl ligula, egestas ut lorem vel, placerat lacinia sapien. Vivamus maximus hendrerit neque, sed consequat felis sagittis ut. Quisque dolor justo, pulvinar et mauris vel, sagittis consectetur eros. Aliquam maximus vestibulum leo sed efficitur. Nam faucibus pharetra nisl nec ullamcorper. Pellentesque rhoncus metus eu enim blandit, sit amet gravida ligula fermentum. Vestibulum mauris erat, tempor ut lacus non, tincidunt fringilla sapien.',
 	}
 
@@ -47,14 +50,18 @@ class Message extends Component {
 		const body = await response.text();
 		this.setState({ text: body });
 	};
+
+	changeTitle = () => {
+		this.setState({ title: "Clicked the title" });
+	}
 	
 	render() {
-		let text = this.state.text;
+		let { sender, time, title, text } = this.state;
 		return (
 			<div className="innerCard">
-				<h2>Bradley Kenny</h2>
-				<p id="time">8:08pm</p>
-				<h3>Information on AirPods Pro</h3>
+				<h2>{ sender }</h2>
+				<p id="time">{ time }</p>
+				<h3 onClick={ this.changeTitle }>{ title }</h3>
 				<p onClick={ this.getter }>{ text }</p>
 				<p></p>
 			</div>
