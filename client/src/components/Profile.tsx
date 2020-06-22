81ef9b36107ee424739b23ce5faafef1185fedb4
bradleykenny <bkenny283@gmail.com>
2020-06-22 11:25:39 +1000

    Profile component started.

---
+++ client/src/components/Profile.tsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+type ProfileProps = {}
+type ProfileState = {}
+
+class Profile extends Component<ProfileProps, ProfileState> {
+	render() {
+		return(
+			<div>
+				<p>Profile</p>
+			</div>
+		);
+	}
+}
+
+export default Profile;
