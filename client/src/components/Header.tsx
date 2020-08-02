b5bcf80f5c7b53e52eea8078deb002df8bf00f20
bradleykenny <bkenny283@gmail.com>
2020-08-02 21:48:50 +1000

    Conversion to functional component.

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,25 +1,31 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import "../style/Header.css";
 
-import { UserData } from '../actions/user';
+import { UserData } from "../actions/user";
+import User from "../types/User";
 
-class Header extends Component<{ user: UserData }, {}> {
-	render() {
-		const { user } = this.props;
-		return (
-			<header>
-				<h1 id="logo">
-					<a href="/" className="headerLogo">communicate</a>
-				</h1>
-				<p>
-					<a href="/">Home</a>
-					<a href="/">{ this.props.user.email }</a>
-					{ localStorage.getItem('sessionID') && <a href="/logout">Logout</a> }
-					{ !localStorage.getItem('sessionID') && <a href="/login">Login</a> }
-				</p>
-			</header>
-		);
-	}
-}
+const Header = (props: { user: UserData }) => {
+	const { user } = props;
+
+	return (
+		<header>
+			<h1 id="logo">
+				<a href="/" className="headerLogo">
+					communicate
+				</a>
+			</h1>
+			<p>
+				<a href="/">Home</a>
+				<a href="/">{props.user.email}</a>
+				{localStorage.getItem("sessionID") && (
+					<a href="/logout">Logout</a>
+				)}
+				{!localStorage.getItem("sessionID") && (
+					<a href="/login">Login</a>
+				)}
+			</p>
+		</header>
+	);
+};
 
 export default Header;
